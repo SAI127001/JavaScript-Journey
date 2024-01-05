a509f20fdabd182835a96809793f365f19d00981
SAI127001 <codesai127.0.0.1@gmail.com>
2024-01-05 21:25:08 +0530

    Jan 5 2024

--- Day 1/Chapter 2/tp.js
+++ Day 1/Chapter 2/tp.js
@@ -1,7 +1,18 @@
-let PName = "Terukula Sai";
+// let PName = "Terukula Sai";
 
-for (let i = 1; i <= PName.length; i++) {
-  console.log(i);
+// for (let i = 1; i <= PName.length; i++) {
+//   console.log(i);
+// }
+
+// console.count(PName);
+
+let n = 2;
+
+switch (n) {
+  case n == 2:
+    return true;
+    break;
+
+  default:
+    break;
 }
-
-console.count(PName);
